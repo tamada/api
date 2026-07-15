6f081b642792850ed107af3e0981cdce243bf86d
Haruaki Tamada <tamada@users.noreply.github.com>
2026-07-15 17:04:57 +0900

    Update GitHub Actions to use v3 of create-github-app-token and specify repositories for token generation

--- scripts/install-pipeline-bot-to-repository/run.sh
+++ scripts/install-pipeline-bot-to-repository/run.sh
@@ -1,33 +1,37 @@
-# App ID を設定（メモした数字）
-APP_ID="4300601"
-
-if [[ "$@" == 1 ]]; then
-    echo "run.sh <PEM>"
+if [[ "$@" != 4 ]]; then
+    echo "run.sh <REPO> <PEM> <CLIENT_ID>"
+    exit 1
+elif [[ ! -f "$2" ]]; then
+    echo "指定されたファイルが存在しません: $2"
+    exit 1
+elif [[ ! "$2" =~ \.pem$ ]]; then
+    echo "指定されたファイルは .pem ファイルではありません: $2"
     exit 1
 fi
 
 # ダウンロードした .pem ファイルのパスを指定
-PEM_FILE=$1
+PEM_FILE=$2
+CLIENT_ID=$3
+REPO=$1
 
 # 連携させたいリポジトリのリスト
-REPOS=(
-  "tamada/fauxrest"
-  "tamada/spellout"
-  "tamada/pick-a-boo"
-  "tamada/oinkie"
-  "tamada/lis"
-  "tamada/gixor"
-  "tamada/totebag"
-  "tamada/btmeister"
-  "tamada/heatman"
-  "tamada/9rules"
-  "tamada/pochi"
-  "tamada/sibling"
-  "tamada/wildcat"
-)
+# REPOS=(
+  # "tamada/fauxrest"
+  # "tamada/spellout"
+  # "tamada/pick-a-boo"
+  # "tamada/oinkie"
+  # "tamada/lis"
+  # "tamada/gixor"
+  # "tamada/totebag"
+  # "tamada/btmeister"
+  # "tamada/heatman"
+  # "tamada/9rules"
+  # "tamada/pochi"
+  # "tamada/sibling"
+  # "tamada/wildcat"
+  # "tamada/api"
+# )
 
-for repo in "${REPOS[@]}"; do
-  gh secret set APP_ID --body "$APP_ID" --repo "$repo"
-  gh secret set APP_PRIVATE_KEY --body "$(cat $PEM_FILE)" --repo "$repo"
-  echo "$repo にシークレットを登録完了！"
-done
+gh secret set CLIENT_ID --body "$CLIENT_ID" --repo "$repo"
+gh secret set APP_PRIVATE_KEY --body "$(cat $PEM_FILE)" --repo "$repo"
+echo "$repo にシークレットを登録完了！"
